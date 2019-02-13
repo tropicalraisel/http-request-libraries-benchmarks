54559108ace400234b60c1d2e7b39a10112876ba
Gevorg A. Galstyan <gevorg@galstyan.am>
2019-02-14 03:24:04 +0400

    Benchmarks for the libs and results in the readme

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# http-request-libraries-benchmarks
-Comparison between http, https, request, node-fetch, axios and got libraries in terms of development and runtime performance
+# HTTP Request Libraries Benchmarks
+
+The code is benchmarking some of the most popular http request approaches and
+libraries.
+
+It compares the native http, https functions and request, node-fetch, axios
+and got libraries.
+
+Here are the results
+
+```bash
+http http GET request x 23,732 ops/sec ±1.77% (79 runs sampled)
+http http POST request x 22,445 ops/sec ±5.24% (76 runs sampled)
+https https GET request x 23,256 ops/sec ±2.68% (80 runs sampled)
+https https POST request x 22,547 ops/sec ±2.54% (76 runs sampled)
+
+request http GET request x 8,864 ops/sec ±4.10% (76 runs sampled)
+request http POST request x 8,207 ops/sec ±2.12% (78 runs sampled)
+request https GET request x 8,406 ops/sec ±2.71% (75 runs sampled)
+request https POST request x 7,553 ops/sec ±1.91% (75 runs sampled)
+
+fetch http GET request x 8,427 ops/sec ±3.82% (73 runs sampled)
+fetch http GET request x 7,447 ops/sec ±3.00% (75 runs sampled)
+fetch https POST request x 8,499 ops/sec ±1.97% (79 runs sampled)
+fetch https POST request x 7,408 ops/sec ±2.12% (77 runs sampled)
+
+axios http GET request x 8,506 ops/sec ±4.34% (75 runs sampled)
+axios http POST request x 8,413 ops/sec ±2.16% (76 runs sampled)
+axios https GET request x 8,880 ops/sec ±2.14% (78 runs sampled)
+axios https POST request x 8,099 ops/sec ±1.32% (81 runs sampled)
+
+got http GET request x 2,831 ops/sec ±5.47% (70 runs sampled)
+got http POST request x 3,246 ops/sec ±2.72% (77 runs sampled)
+got https GET request x 3,234 ops/sec ±2.10% (80 runs sampled)
+got https POST request x 3,200 ops/sec ±2.21% (79 runs sampled)
+
+Fastest is http http GET request
+```
---
+++ index.ts
@@ -0,0 +1,218 @@
+import Benchmark, { Event } from 'benchmark';
+import http from 'http';
+import https from 'https';
+import nock from 'nock';
+import request from 'request';
+
+import axios from 'axios';
+import got from 'got';
+import fetch from 'node-fetch';
+
+const HOST = 'benchmark';
+const HTTP_BASE_URL = `http://${HOST}`;
+const HTTPS_BASE_URL = `https://${HOST}`;
+
+const PATH = '/run';
+
+const RESPONSE_BODY = JSON.stringify({ message: 'Hello world' });
+
+nock(HTTP_BASE_URL)
+  .persist()
+  .get(PATH)
+  .reply(200, RESPONSE_BODY)
+  .post(PATH)
+  .reply(200, RESPONSE_BODY);
+
+nock(HTTPS_BASE_URL)
+  .persist()
+  .get(PATH)
+  .reply(200, RESPONSE_BODY)
+  .post(PATH)
+  .reply(200, RESPONSE_BODY);
+
+const suite = new Benchmark.Suite();
+
+// http
+suite.add('http http GET request', {
+  defer: true,
+  fn: (defer: any) => {
+    http
+      .request({ host: HOST, path: PATH }, (res) => {
+        res.resume().on('end', () => defer.resolve());
+      })
+      .end();
+  },
+});
+suite.add('http http POST request', {
+  defer: true,
+  fn: (defer: any) => {
+    const req = http.request(
+      { host: HOST, path: PATH, method: 'POST' },
+      (res) => {
+        res.resume().on('end', () => defer.resolve());
+      },
+    );
+    // req.write('');
+    req.end();
+  },
+});
+
+// https
+suite.add('https https GET request', {
+  defer: true,
+  fn: (defer: any) => {
+    https
+      .request({ host: HOST, path: PATH }, (res) => {
+        res.resume().on('end', () => defer.resolve());
+      })
+      .end();
+  },
+});
+suite.add('https https POST request', {
+  defer: true,
+  fn: (defer: any) => {
+    const req = https.request(
+      { host: HOST, path: PATH, method: 'POST' },
+      (res) => {
+        res.resume().on('end', () => defer.resolve());
+      },
+    );
+    // req.write('');
+    req.end();
+  },
+});
+
+// request
+suite.add('request http GET request', {
+  defer: true,
+  fn: (defer: any) => {
+    request.get(HTTP_BASE_URL, () => {
+      defer.resolve();
+    });
+  },
+});
+suite.add('request http POST request', {
+  defer: true,
+  fn: (defer: any) => {
+    request.post(HTTP_BASE_URL, () => {
+      defer.resolve();
+    });
+  },
+});
+
+suite.add('request https GET request', {
+  defer: true,
+  fn: (defer: any) => {
+    request.get(HTTPS_BASE_URL, () => {
+      defer.resolve();
+    });
+  },
+});
+suite.add('request https POST request', {
+  defer: true,
+  fn: (defer: any) => {
+    request.post(HTTPS_BASE_URL, () => {
+      defer.resolve();
+    });
+  },
+});
+
+// fetch
+suite.add('fetch http GET request', {
+  defer: true,
+  fn: (defer: any) => {
+    fetch(`${HTTP_BASE_URL}${PATH}`)
+      .then((res) => res.json())
+      .then(() => defer.resolve());
+  },
+});
+suite.add('fetch http GET request', {
+  defer: true,
+  fn: (defer: any) => {
+    fetch(`${HTTP_BASE_URL}${PATH}`, { method: 'POST' })
+      .then((res) => res.json())
+      .then(() => defer.resolve());
+  },
+});
+
+suite.add('fetch https POST request', {
+  defer: true,
+  fn: (defer: any) => {
+    fetch(`${HTTPS_BASE_URL}${PATH}`)
+      .then((res) => res.json())
+      .then(() => defer.resolve());
+  },
+});
+suite.add('fetch https POST request', {
+  defer: true,
+  fn: (defer: any) => {
+    fetch(`${HTTPS_BASE_URL}${PATH}`, { method: 'POST' })
+      .then((res) => res.json())
+      .then(() => defer.resolve());
+  },
+});
+
+// axios
+suite.add('axios http GET request', {
+  defer: true,
+  fn: (defer: any) => {
+    axios.get(`${HTTP_BASE_URL}${PATH}`).then(() => defer.resolve());
+  },
+});
+suite.add('axios http POST request', {
+  defer: true,
+  fn: (defer: any) => {
+    axios.post(`${HTTP_BASE_URL}${PATH}`).then(() => defer.resolve());
+  },
+});
+
+suite.add('axios https GET request', {
+  defer: true,
+  fn: (defer: any) => {
+    axios.get(`${HTTPS_BASE_URL}${PATH}`).then(() => defer.resolve());
+  },
+});
+suite.add('axios https POST request', {
+  defer: true,
+  fn: (defer: any) => {
+    axios.post(`${HTTPS_BASE_URL}${PATH}`).then(() => defer.resolve());
+  },
+});
+
+// got
+suite.add('got http GET request', {
+  defer: true,
+  fn: (defer: any) => {
+    got.get(`${HTTP_BASE_URL}${PATH}`).then(() => defer.resolve());
+  },
+});
+suite.add('got http POST request', {
+  defer: true,
+  fn: (defer: any) => {
+    got.post(`${HTTP_BASE_URL}${PATH}`).then(() => defer.resolve());
+  },
+});
+
+suite.add('got https GET request', {
+  defer: true,
+  fn: (defer: any) => {
+    got.get(`${HTTPS_BASE_URL}${PATH}`).then(() => defer.resolve());
+  },
+});
+suite.add('got https POST request', {
+  defer: true,
+  fn: (defer: any) => {
+    got.post(`${HTTPS_BASE_URL}${PATH}`).then(() => defer.resolve());
+  },
+});
+
+suite
+  .on('cycle', (event: Event) => {
+    // tslint:disable-next-line: no-console
+    console.log(event.target.toString());
+  })
+  .on('complete', () => {
+    // tslint:disable-next-line: no-console
+    console.log(`Fastest is ${suite.filter('fastest')[0].name}`);
+  })
+  .run({ async: true });
